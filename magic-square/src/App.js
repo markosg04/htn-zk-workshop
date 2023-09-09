73524fe082de34cc71f792cb8c57424cc6b08450
markosg04 <markos@quantstamp.com>
2023-09-09 14:17:49 -0400

    WIP

---
+++ magic-square/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MagicSquareVerifier from './MagicSquareVerifier';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MagicSquareVerifier />
+      </header>
+    </div>
+  );
+}
+
+export default App;
